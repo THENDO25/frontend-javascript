65bd27713fbd95d360ee6adb0c9117d383e4a919
THENDO25 <thendomukwevho1125@gmail.com>
2024-12-11 17:04:38 +0200

    update

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,19 +1,22 @@
-namespace Subjects {
-    export interface Teacher {
-      experienceTeachingJava?: number;
-    }
-  
-    export class Java extends Subject {
-      getRequirements(): string {
-        return 'Here is the list of requirements for Java';
-      }
-  
-      getAvailableTeacher(): string {
-        if (this._teacher && this._teacher.experienceTeachingJava) {
-          return `Available Teacher: ${this._teacher.firstName}`;
-        } else {
-          return 'No available teacher';
-        }
-      }
+// Java.ts
+import { Teacher } from './Teacher';
+
+export class Java {
+  private _teacher: Teacher | null = null;
+
+  setTeacher(teacher: Teacher) {
+    this._teacher = teacher;
+  }
+
+  getRequirements(): string {
+    return 'Here is the list of requirements for Java';
+  }
+
+  getAvailableTeacher(): string {
+    if (this._teacher && this._teacher.experienceTeachingJava) {
+      return `Available Teacher: ${this._teacher.firstName}`;
+    } else {
+      return 'No available teacher';
     }
   }
+}
