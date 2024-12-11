3f23ea939c19ec3869745d4941a7d9edda067c59
THENDO25 <thendomukwevho1125@gmail.com>
2024-12-11 15:08:42 +0200

    4th commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,15 @@ interface Teacher {
     (firstName: string, lastName: string): string;
   }
   
+  interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+  
+  interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
   const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
   };
@@ -35,6 +44,27 @@ interface Teacher {
     numberOfReports: 17,
   };
   
+  class StudentClass {
+    private firstName: string;
+    private lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+  
+  const student = new StudentClass('John', 'Doe');
   console.log(teacher3);
   console.log(director1);
   console.log(printTeacher(director1.firstName, director1.lastName)); // J. Doe
+  console.log(student.workOnHomework()); // Currently working
+  console.log(student.displayName()); // John
