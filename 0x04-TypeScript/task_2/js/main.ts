8efa641b0fd7beb39cb4da2bead2230663485f3c
THENDO25 <thendomukwevho1125@gmail.com>
2024-12-11 15:11:35 +0200

    5th commit

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -46,6 +46,21 @@ interface DirectorInterface {
     }
   }
   
+  function isDirector(employee: Director | Teacher): employee is Director {
+    return 'workDirectorTasks' in employee;
+  }
+  
+  function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+    } else {
+      return employee.workTeacherTasks();
+    }
+  }
+  
   console.log(createEmployee(200)); // Teacher
   console.log(createEmployee(1000)); // Director
   console.log(createEmployee('$500')); // Director
+  
+  console.log(executeWork(createEmployee(200))); // Getting to work
+  console.log(executeWork(createEmployee(1000))); // Getting to director tasks
